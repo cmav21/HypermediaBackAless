bbbfc61cb403a2ebadd5952139b561824ac8cea8
cesar magallon <cesar9mv@gmail.com>
2019-08-18 14:20:11 -0500

    [deploy]deploy

--- app.js
+++ app.js
@@ -1,39 +1,18 @@
-// const argv = require("yargs")
-// .command('serve [port]', 'start the server', {
-//     base:{
-//         alias:'b',
-//     },
-//     altura:{
-//         alias:'a',
-//     }
-//   })
-//   .demandOption('base')
-//   .default({
-//     'base':10,
-//     'altura':10
-//   })
-//   .help()
-//   .argv
+const http = require("http");
 
-const argv = require("yargs")
-    .command('lista', "listar tabla de multiplicar", {
-        base:{
-            alias: 'b',
-            demand: true
-        }
-    })
-    .help()
-    .argv
+http.createServer((req, res) => {
+    
+    res.writeHead(200, {'Content-Type': 'application/json'});
 
-let comando = argv._[0];
-switch (comando) {
-    case 'listar':
-        console.log('listar');
-        break;
-    case 'crear':
-        console.log('crear');
-        break;
-    default:
-        console.log('comando no reconocido')
-        break;
-}
+    let salida = {
+        nombre:'cesar',
+        edad:20,
+        url: req.url
+    }
+
+    res.write(JSON.stringify(salida));
+    //always have to be indicated when we already send the response
+    res.end();
+}).listen(8080);
+
+console.log("Escuchando el puerto 8080");
